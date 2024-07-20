0a5a1c7b161e5284b7cde88f930c010059eea5a9
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-07-20 14:22:04 +0900

    feat: @JsonIgnoreProperties로, 객체에 없는 필드는 변환 무시

--- src/main/java/com/hcpark/tobyspring6/ExchangeRateInfo.java
+++ src/main/java/com/hcpark/tobyspring6/ExchangeRateInfo.java
@@ -3,5 +3,8 @@ package com.hcpark.tobyspring6;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ExchangeRateInfo(String result, Map<String, BigDecimal> rates) {
 }
