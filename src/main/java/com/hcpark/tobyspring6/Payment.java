9f3a159a1681a1cd151a60a07eaa2ad655ce735e
hy2850 <hy2850@gmail.com>
2024-07-20 13:48:25 +0900

    feat: Payment DTO에 필드 추가

--- src/main/java/com/hcpark/tobyspring6/Payment.java
+++ src/main/java/com/hcpark/tobyspring6/Payment.java
@@ -3,5 +3,5 @@ package com.hcpark.tobyspring6;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record Payment(BigDecimal exchangeRate, BigDecimal convertedPayAmount, LocalDateTime validUntil) {
+public record Payment(Long orderId, String currency, BigDecimal payAmount, BigDecimal exchangeRate, BigDecimal convertedPayAmount, LocalDateTime validUntil) {
 }
