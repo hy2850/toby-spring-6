7e35a21d5c3fcbc1278f27c907151e9b9657591c
hy2850 <hy2850@gmail.com>
2024-07-20 13:39:02 +0900

    feat: PaymentService.prepare 메소드 선언

--- src/main/java/com/hcpark/tobyspring6/PaymentService.java
+++ src/main/java/com/hcpark/tobyspring6/PaymentService.java
@@ -1,8 +1,16 @@
 package com.hcpark.tobyspring6;
 
+import java.math.BigDecimal;
+
 public class PaymentService {
 
-    public static Payment prepare() {
+    public static Payment prepare(Long orderId, String currency, BigDecimal payAmount) {
+        // 환율 정보 API 호출
+
+        // 환율 정보 JSON 변환
+
+        // 환율 계산
+
         return new Payment();
     }
 
