fb57a29ce2a1adb53bfbe1cc1a216a4312baeac5
hy2850 <hy2850@gmail.com>
2024-07-20 12:46:41 +0900

    Init project

---
+++ src/main/java/com/hcpark/tobyspring6/TobySpring6Application.java
@@ -0,0 +1,13 @@
+package com.hcpark.tobyspring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpring6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpring6Application.class, args);
+    }
+
+}
