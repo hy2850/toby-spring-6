cea14cf40cccc7d305909865aa8d5b3ac682bb33
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-07-30 23:55:39 +0900

    refactor: PaymentService 구현하는 클래스 개발

---
+++ src/main/java/com/hcpark/tobyspring6/PaymentServiceUSD.java
@@ -0,0 +1,38 @@
+package com.hcpark.tobyspring6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class PaymentServiceUSD extends PaymentService {
+
+    public PaymentServiceUSD(Long orderId, String currency, BigDecimal payAmount) {
+        super(orderId, currency, payAmount);
+    }
+
+    @Override
+    BigDecimal getExchangeRate() throws IOException {
+        // 환율 정보 API 호출
+        var url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        var connection = (HttpURLConnection) url.openConnection();
+        var br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        var response = br.lines().collect(Collectors.joining());
+
+        // 환율 정보 JSON 변환
+        var objectMapper = new ObjectMapper();
+        var exchangeRateInfo = objectMapper.readValue(response, ExchangeRateInfo.class);
+        return exchangeRateInfo.rates().get(TARGET_CURRENCY);
+    }
+
+    public static void main(String[] args) throws IOException {
+        var paymentService = new PaymentServiceUSD(123L, "USD", BigDecimal.valueOf(100));
+        var payment = paymentService.prepare();
+        System.out.println(payment);
+    }
+}
