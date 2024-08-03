bc2b84f762e56b51511d18c1dcde22fa8f45621e
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-08-03 16:19:57 +0900

    refactor: PaymentService abstract class에서 인스턴스 변수 제거

--- src/main/java/com/hcpark/tobyspring6/PaymentService.java
+++ src/main/java/com/hcpark/tobyspring6/PaymentService.java
@@ -6,41 +6,10 @@ import java.time.LocalDateTime;
 
 public abstract class PaymentService {
 
-    Long orderId;
-    String currency;
-    BigDecimal payAmount;
-
     static String TARGET_CURRENCY = "KRW";
 
-    public PaymentService(Long orderId, String currency, BigDecimal payAmount) {
-        this.orderId = orderId;
-        this.currency = currency;
-        this.payAmount = payAmount;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public BigDecimal getPayAmount() {
-        return payAmount;
-    }
-
-    @Override
-    public String toString() {
-        return "PaymentService{" +
-            "orderId=" + orderId +
-            ", currency='" + currency + '\'' +
-            ", payAmount=" + payAmount +
-            '}';
-    }
-
-    public Payment prepare() throws IOException {
-        var exchangeRate = getExchangeRate();
+    public Payment prepare(Long orderId, String currency, BigDecimal payAmount) throws IOException {
+        var exchangeRate = getExchangeRate(currency);
 
         // 환율 계산
         var convertedPayAmount = payAmount.multiply(exchangeRate);
@@ -49,5 +18,5 @@ public abstract class PaymentService {
         return new Payment(orderId, currency, payAmount, exchangeRate, convertedPayAmount, validUntil);
     }
 
-    abstract BigDecimal getExchangeRate() throws IOException;
+    abstract BigDecimal getExchangeRate(String currency) throws IOException;
 }
