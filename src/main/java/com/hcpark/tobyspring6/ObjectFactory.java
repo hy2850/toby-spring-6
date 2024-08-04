4337158344823e889358b45ea4026e26e2cccc51
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-08-04 19:56:57 +0900

    feat: @ComponentScan을 활용한 빈 등록 및 의존관계 셋팅

--- src/main/java/com/hcpark/tobyspring6/ObjectFactory.java
+++ src/main/java/com/hcpark/tobyspring6/ObjectFactory.java
@@ -1,19 +1,20 @@
 package com.hcpark.tobyspring6;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
 
-    @Bean
-    public PaymentService getPaymentService() {
-        return new PaymentService(getExRateProvider());
-    }
-
-    @Bean
-    private static ExRateProvider getExRateProvider() {
-        return new SimpleExRateProvider();
-//        return new WebApiExRateProvider();
-    }
+//    @Bean
+//    public PaymentService getPaymentService() {
+//        return new PaymentService(getExRateProvider());
+//    }
+//
+//    @Bean
+//    private static ExRateProvider getExRateProvider() {
+//        return new SimpleExRateProvider();
+////        return new WebApiExRateProvider();
+//    }
 }
