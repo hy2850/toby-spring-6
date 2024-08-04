1198a7d76c58d58baabc1587fb7145c705c0848c
hy2850 <hy2850@gmail.com>
2024-08-04 20:07:16 +0900

    싱글톤 레지스트리 - beanFactory 객체가 싱글턴인지 체크

--- src/main/java/com/hcpark/tobyspring6/PaymentService.java
+++ src/main/java/com/hcpark/tobyspring6/PaymentService.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import org.springframework.stereotype.Component;
-
-@Component
+//@Component
 public class PaymentService {
 
     private final ExRateProvider exRateProvider;
