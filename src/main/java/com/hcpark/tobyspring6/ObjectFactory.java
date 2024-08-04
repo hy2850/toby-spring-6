82cacaea0c827243a5d048d72caef354a9d7b4ba
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-08-04 17:08:58 +0900

    fix: ExRateProvider 가져오는 관심사도 별도 메소드로 분리

--- src/main/java/com/hcpark/tobyspring6/ObjectFactory.java
+++ src/main/java/com/hcpark/tobyspring6/ObjectFactory.java
@@ -3,6 +3,11 @@ package com.hcpark.tobyspring6;
 public class ObjectFactory {
 
     public PaymentService getPaymentService() {
-        return new PaymentService(new WebApiExRateProvider());
+        return new PaymentService(getExRateProvider());
+    }
+
+    private static ExRateProvider getExRateProvider() {
+//        return new SimpleExRateProvider();
+        return new WebApiExRateProvider();
     }
 }
