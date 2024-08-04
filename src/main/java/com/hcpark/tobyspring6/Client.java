7add2fe9892a86c738d8053efcbe09026c146fcc
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-08-04 19:55:42 +0900

    오브젝트 팩토리 - Spring bean factory 도입

--- src/main/java/com/hcpark/tobyspring6/Client.java
+++ src/main/java/com/hcpark/tobyspring6/Client.java
@@ -3,10 +3,13 @@ package com.hcpark.tobyspring6;
 import java.io.IOException;
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Client {
     public static void main(String[] args) throws IOException {
-        var objectFactory = new ObjectFactory();
-        var paymentService = objectFactory.getPaymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        var paymentService = beanFactory.getBean(PaymentService.class);
         var payment = paymentService.prepare(123L, "USD", BigDecimal.valueOf(100));
         System.out.println(payment);
     }
