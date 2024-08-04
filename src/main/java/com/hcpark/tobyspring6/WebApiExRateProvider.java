df9c66d4f22231d48731802a20bceec5d3bf97ae
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-08-04 16:30:35 +0900

    feat: 인터페이스 도입
    그럼에도 불구하고 PaymentService -> ExRateProvider 구체에 강하게 의존

--- src/main/java/com/hcpark/tobyspring6/WebApiExRateProvider.java
+++ src/main/java/com/hcpark/tobyspring6/WebApiExRateProvider.java
@@ -10,11 +10,12 @@ import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class WebApiExRateProvider {
+public class WebApiExRateProvider implements ExRateProvider {
 
     private static String TARGET_CURRENCY = "KRW";
 
-    BigDecimal getWebExchangeRate(String currency) throws IOException {
+    @Override
+    public BigDecimal getExchangeRate(String currency) throws IOException {
         var url = new URL("https://open.er-api.com/v6/latest/" + currency);
         var connection = (HttpURLConnection) url.openConnection();
         var br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
