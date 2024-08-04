374c6344c43690bf18e2cae77d5485785ece4c04
hy2850 <hy2850@gmail.com>
2024-08-04 19:55:42 +0900

    오브젝트 팩토리 - Spring bean factory 도입

--- src/main/java/com/hcpark/tobyspring6/ObjectFactory.java
+++ src/main/java/com/hcpark/tobyspring6/ObjectFactory.java
@@ -1,13 +1,19 @@
 package com.hcpark.tobyspring6;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService getPaymentService() {
         return new PaymentService(getExRateProvider());
     }
 
+    @Bean
     private static ExRateProvider getExRateProvider() {
-//        return new SimpleExRateProvider();
-        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
+//        return new WebApiExRateProvider();
     }
 }
