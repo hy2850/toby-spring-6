de06b3017b27190bd2014389187aa6d2dd7796a5
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-08-04 16:44:56 +0900

    feat: 구체 생성 책임을 Client로 넘기기

--- src/main/java/com/hcpark/tobyspring6/PaymentService.java
+++ src/main/java/com/hcpark/tobyspring6/PaymentService.java
@@ -8,10 +8,8 @@ public class PaymentService {
 
     private final ExRateProvider exRateProvider;
 
-    public PaymentService() {
-        // 구체에 강하게 의존 -> 사용하고 싶은 구체를 바꾸고 싶다면 여기 코드를 바꿔야; 확장성 떨어짐
-//        this.exRateProvider = new WebApiExRateProvider();
-        this.exRateProvider = new SimpleExRateProvider();
+    public PaymentService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal payAmount) throws IOException {
