ad308d27f07154c2256eb5bdccf17991eeb707b3
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-09-06 18:10:51 +0900

    DI와 디자인패턴 - 캐시 timeout 적용 및 Client 코드에서 테스트

--- src/main/java/com/hcpark/tobyspring6/Client.java
+++ src/main/java/com/hcpark/tobyspring6/Client.java
@@ -2,12 +2,13 @@ package com.hcpark.tobyspring6;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
 
 //        var paymentService = beanFactory.getBean(PaymentService.class);
@@ -19,7 +20,17 @@ public class Client {
         var paymentService2 = objectFactory.getPaymentService();
         System.out.println(paymentService == paymentService2); // true
 
-        var payment = paymentService.prepare(123L, "USD", BigDecimal.valueOf(100));
-        System.out.println(payment);
+        var payment1 = paymentService.prepare(123L, "USD", BigDecimal.valueOf(100));
+        System.out.println(payment1);
+
+        TimeUnit.SECONDS.sleep(1);
+
+        var payment2 = paymentService.prepare(123L, "USD", BigDecimal.valueOf(100));
+        System.out.println(payment2);
+
+        TimeUnit.SECONDS.sleep(3);
+
+        var payment3 = paymentService.prepare(123L, "USD", BigDecimal.valueOf(100));
+        System.out.println(payment3);
     }
 }
