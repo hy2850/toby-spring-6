accb305705e8a3377483de6c117b702beb83844f
hy2850 <hy2850@gmail.com>
2024-09-06 18:04:45 +0900

    DI와 디자인패턴 - 캐시값 저장할 인스턴스 변수 생성

--- src/main/java/com/hcpark/tobyspring6/CachedExRateProvider.java
+++ src/main/java/com/hcpark/tobyspring6/CachedExRateProvider.java
@@ -2,10 +2,12 @@ package com.hcpark.tobyspring6;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class CachedExRateProvider implements ExRateProvider {
 
     private final ExRateProvider target;
+    private BigDecimal cachedExchangeRate;
 
     public CachedExRateProvider(ExRateProvider target) {
         this.target = target;
@@ -13,6 +15,11 @@ public class CachedExRateProvider implements ExRateProvider {
 
     @Override
     public BigDecimal getExchangeRate(String currency) throws IOException {
+        if (Objects.isNull(cachedExchangeRate)) {
+            cachedExchangeRate = target.getExchangeRate(currency);
+            return cachedExchangeRate;
+        }
+
         return target.getExchangeRate(currency);
     }
 }
