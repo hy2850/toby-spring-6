24c54bad96307a81169a7cc634aee6fe7dfe2706
hy2850 <hy2850@gmail.com>
2024-09-07 12:13:01 +0900

    테스트 Junit 테스트 작성 - sortByLength 테스트

--- src/main/java/com/hcpark/tobyspring6/Sort.java
+++ src/main/java/com/hcpark/tobyspring6/Sort.java
@@ -1,24 +1,12 @@
 package com.hcpark.tobyspring6;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Sort {
 
-    public static void main(String[] args) {
-        List<String> arr = Arrays.asList("a", "b", "spring", "java");
-
-//        Collections.sort(arr, new Comparator<String>() {
-//            @Override
-//            public int compare(String o1, String o2) {
-//                return o1.length() - o2.length();
-//            }
-//        });
-
+    public List<String> sortByLength(List<String> list) {
         // Comparator 인스턴스를 람다 표현식으로 구현
-        Collections.sort(arr, (str1, str2) -> str1.length() - str2.length());
-
-        arr.forEach(System.out::println);
+        list.sort((str1, str2) -> str1.length() - str2.length());
+        return list;
     }
 }
