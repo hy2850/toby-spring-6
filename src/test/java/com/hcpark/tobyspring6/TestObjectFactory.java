aea892cad1e6677a3279e932787c5c99e9b41280
hy2850 <hy2850@gmail.com>
2024-09-07 18:55:59 +0900

    테스트와 DI (2) - stub DI를 스프링 테스트 컨테이너 통해서

---
+++ src/test/java/com/hcpark/tobyspring6/TestObjectFactory.java
@@ -0,0 +1,24 @@
+package com.hcpark.tobyspring6;
+
+import java.math.BigDecimal;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hcpark.tobyspring6.payment.ExRateProvider;
+import com.hcpark.tobyspring6.payment.ExRateProviderStub;
+import com.hcpark.tobyspring6.payment.PaymentService;
+
+@Configuration
+public class TestObjectFactory {
+
+    @Bean
+    public PaymentService getPaymentService() {
+        return new PaymentService(getExRateProviderStub());
+    }
+
+    @Bean
+    private static ExRateProvider getExRateProviderStub() {
+        return new ExRateProviderStub(BigDecimal.valueOf(1_000));
+    }
+}
