493bed3b9a18eb3ea500286fedabd7a86e8b57d0
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-09-07 18:26:45 +0900

    테스트와 DI (1) - 메소드 추출해서 여러 파라미터로 테스트 돌릴 수 있도록

--- src/test/java/com/hcpark/tobyspring6/payment/PaymentServiceTest.java
+++ src/test/java/com/hcpark/tobyspring6/payment/PaymentServiceTest.java
@@ -1,7 +1,5 @@
 package com.hcpark.tobyspring6.payment;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -39,21 +37,23 @@ class PaymentServiceTest {
     @Test
     @DisplayName("ExRateProviderStub 사용해서 PaymentService 비즈니스 로직 검증")
     void prepareWithStub() throws IOException {
+        testConvertedAmount(BigDecimal.valueOf(100), BigDecimal.valueOf(1_000));
+        testConvertedAmount(BigDecimal.valueOf(300), BigDecimal.valueOf(3_000));
+        testConvertedAmount(BigDecimal.valueOf(500), BigDecimal.valueOf(5_000));
+    }
+
+    private void testConvertedAmount(BigDecimal exRateInput, BigDecimal convertedAmountToTest) throws IOException {
         // given
-        var paymentService = new PaymentService(new ExRateProviderStub(BigDecimal.valueOf(500)));
+        var paymentService = new PaymentService(new ExRateProviderStub(exRateInput));
 
         // when
         var payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
         // then
         // 1. 환율 정보 가져오기
-        Assertions.assertThat(payment.exchangeRate()).isEqualTo(BigDecimal.valueOf(500));
+        Assertions.assertThat(payment.exchangeRate()).isEqualTo(exRateInput);
 
         // 2. 원화 환산 금액 맞는지
-        Assertions.assertThat(payment.convertedPayAmount()).isEqualTo(BigDecimal.valueOf(5_000));
-
-        // 3. 원화 환산 금액의 유효시간 유효한지
-        Assertions.assertThat(payment.validUntil()).isAfter(LocalDateTime.now());
-        Assertions.assertThat(payment.validUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        Assertions.assertThat(payment.convertedPayAmount()).isEqualTo(convertedAmountToTest);
     }
 }
