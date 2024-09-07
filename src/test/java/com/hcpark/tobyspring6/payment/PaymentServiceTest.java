15c1ac295e53fb9313b0a810e8acc1e8bfdc09d4
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-09-07 12:44:07 +0900

    테스트와 DI (1) - ExRateProviderStub 도입

--- src/test/java/com/hcpark/tobyspring6/payment/PaymentServiceTest.java
+++ src/test/java/com/hcpark/tobyspring6/payment/PaymentServiceTest.java
@@ -35,4 +35,25 @@ class PaymentServiceTest {
         Assertions.assertThat(payment.validUntil()).isAfter(LocalDateTime.now());
         Assertions.assertThat(payment.validUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
     }
+
+    @Test
+    @DisplayName("ExRateProviderStub 사용해서 PaymentService 비즈니스 로직 검증")
+    void prepareWithStub() throws IOException {
+        // given
+        var paymentService = new PaymentService(new ExRateProviderStub(BigDecimal.valueOf(500)));
+
+        // when
+        var payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // then
+        // 1. 환율 정보 가져오기
+        Assertions.assertThat(payment.exchangeRate()).isEqualTo(BigDecimal.valueOf(500));
+
+        // 2. 원화 환산 금액 맞는지
+        Assertions.assertThat(payment.convertedPayAmount()).isEqualTo(BigDecimal.valueOf(5_000));
+
+        // 3. 원화 환산 금액의 유효시간 유효한지
+        Assertions.assertThat(payment.validUntil()).isAfter(LocalDateTime.now());
+        Assertions.assertThat(payment.validUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
 }
