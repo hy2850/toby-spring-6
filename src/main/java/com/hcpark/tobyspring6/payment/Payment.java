ffa576faa11ca68497f7186ade46652290c14121
hy2850 <hy2850@gmail.com>
2024-09-08 16:43:18 +0900

    도메인 오브젝트 테스트 - convertedPayAmount와 validUntil 로직을 Payment 도메인 객체 내부로 이동

--- src/main/java/com/hcpark/tobyspring6/payment/Payment.java
+++ src/main/java/com/hcpark/tobyspring6/payment/Payment.java
@@ -1,7 +1,15 @@
 package com.hcpark.tobyspring6.payment;
 
 import java.math.BigDecimal;
+import java.time.Clock;
 import java.time.LocalDateTime;
 
 public record Payment(Long orderId, String currency, BigDecimal payAmount, BigDecimal exchangeRate, BigDecimal convertedPayAmount, LocalDateTime validUntil) {
+
+    public static Payment createPayment(Long orderId, String currency, BigDecimal payAmount, BigDecimal exchangeRate, Clock clock) {
+        var convertedPayAmount = payAmount.multiply(exchangeRate);
+        var validUntil = LocalDateTime.now(clock).plusMinutes(30);
+
+        return new Payment(orderId, currency, payAmount, exchangeRate, convertedPayAmount, validUntil);
+    }
 }
--- src/main/java/com/hcpark/tobyspring6/payment/PaymentService.java
+++ src/main/java/com/hcpark/tobyspring6/payment/PaymentService.java
@@ -3,7 +3,6 @@ package com.hcpark.tobyspring6.payment;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.Clock;
-import java.time.LocalDateTime;
 
 //@Component
 public class PaymentService {
@@ -19,9 +18,6 @@ public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal payAmount) throws IOException {
         var exchangeRate = exRateProvider.getExchangeRate(currency);
 
-        var convertedPayAmount = payAmount.multiply(exchangeRate);
-        var validUntil = LocalDateTime.now(this.clock).plusMinutes(30);
-
-        return new Payment(orderId, currency, payAmount, exchangeRate, convertedPayAmount, validUntil);
+        return Payment.createPayment(orderId, currency, payAmount, exchangeRate, this.clock);
     }
 }
