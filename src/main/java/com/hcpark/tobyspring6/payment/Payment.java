dde1263cba64890b2e5ed4398c796d652f344a65
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-09-08 17:04:10 +0900

    도메인 오브젝트 테스트 - Payment.isValid 비즈니스 로직 추가 & 테스트

--- src/main/java/com/hcpark/tobyspring6/payment/Payment.java
+++ src/main/java/com/hcpark/tobyspring6/payment/Payment.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.LocalDateTime;
 
+// Payment 기본 constructor를 private으로 만들고, createPayment 객체로만 생성할 수 있게 하는게 맞다. 리팩토링 귀찮아서 스킵
 public record Payment(Long orderId, String currency, BigDecimal payAmount, BigDecimal exchangeRate, BigDecimal convertedPayAmount, LocalDateTime validUntil) {
 
     public static Payment createPayment(Long orderId, String currency, BigDecimal payAmount, BigDecimal exchangeRate, Clock clock) {
@@ -11,5 +12,9 @@ public record Payment(Long orderId, String currency, BigDecimal payAmount, BigDe
         var validUntil = LocalDateTime.now(clock).plusMinutes(30);
 
         return new Payment(orderId, currency, payAmount, exchangeRate, convertedPayAmount, validUntil);
+    }
+
+    public boolean isValid(Clock clock) {
+        return LocalDateTime.now(clock).isBefore(validUntil);
     }
 }
--- src/test/java/com/hcpark/tobyspring6/payment/PaymentTest.java
+++ src/test/java/com/hcpark/tobyspring6/payment/PaymentTest.java
@@ -2,6 +2,7 @@ package com.hcpark.tobyspring6.payment;
 
 import java.math.BigDecimal;
 import java.time.Clock;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -34,5 +35,18 @@ public class PaymentTest {
         // then
         Assertions.assertThat(payment.convertedPayAmount()).isEqualByComparingTo(payAmount.multiply(exchangeRate));
         Assertions.assertThat(payment.validUntil()).isEqualTo(LocalDateTime.now(this.clock).plusMinutes(30));
+    }
+
+    @Test
+    void testIsValid() {
+        // given
+        var payAmount = BigDecimal.TEN;
+        var exchangeRate = BigDecimal.valueOf(1_000);
+        var payment = Payment.createPayment(1L, "USD", payAmount, exchangeRate, this.clock);
+
+        // when, then
+        Assertions.assertThat(payment.isValid(this.clock)).isTrue();
+        Assertions.assertThat(payment.isValid(Clock.offset(this.clock, Duration.ofMinutes(29)))).isTrue();
+        Assertions.assertThat(payment.isValid(Clock.offset(this.clock, Duration.ofMinutes(30)))).isFalse();
     }
 }
