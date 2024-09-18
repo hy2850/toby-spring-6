d87aae78933bf19303ee158d1d2b1f26c8025bb5
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-09-18 12:09:58 +0900

    ApiTemplate 분리 - 바뀌지 않는 템플릿을 구체 클래스로 분리

---
+++ src/main/java/com/hcpark/tobyspring6/api/ApiTemplate.java
@@ -0,0 +1,39 @@
+package com.hcpark.tobyspring6.api;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.hcpark.tobyspring6.exrate.ExchangeRateInfo;
+
+public class ApiTemplate {
+
+    private static String TARGET_CURRENCY = "KRW";
+
+    public static BigDecimal getExchangeRateWithApi(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+        URI uri;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String response;
+        try {
+            response = apiExecutor.executeApi(uri);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ExchangeRateInfo exchangeRateInfo;
+        try {
+            exchangeRateInfo = exRateExtractor.extractExRateFrom(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return exchangeRateInfo.rates().get(TARGET_CURRENCY);
+    }
+}
