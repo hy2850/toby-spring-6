4e9e15f9e62eb9e47764d7675f72ca43712ca398
hy2850 <hy2850@gmail.com>
2024-09-18 12:09:58 +0900

    ApiTemplate 분리 - 바뀌지 않는 템플릿을 구체 클래스로 분리

--- src/main/java/com/hcpark/tobyspring6/exrate/WebApiExRateProvider.java
+++ src/main/java/com/hcpark/tobyspring6/exrate/WebApiExRateProvider.java
@@ -1,52 +1,18 @@
 package com.hcpark.tobyspring6.exrate;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hcpark.tobyspring6.api.ApiExecutor;
+import com.hcpark.tobyspring6.api.ApiTemplate;
 import com.hcpark.tobyspring6.api.ErApiExRateExtractor;
-import com.hcpark.tobyspring6.api.ExRateExtractor;
 import com.hcpark.tobyspring6.api.SimpleApiExecutor;
 import com.hcpark.tobyspring6.payment.ExRateProvider;
 
 //@Component
 public class WebApiExRateProvider implements ExRateProvider {
 
-    private static String TARGET_CURRENCY = "KRW";
-
     @Override
     public BigDecimal getExchangeRate(String currency) {
         var url = "https://open.er-api.com/v6/latest/" + currency;
-
-        return getExchangeRateWithApi(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
-    }
-
-    private static BigDecimal getExchangeRateWithApi(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.executeApi(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        ExchangeRateInfo exchangeRateInfo;
-        try {
-            exchangeRateInfo = exRateExtractor.extractExRateFrom(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-        return exchangeRateInfo.rates().get(TARGET_CURRENCY);
+        return ApiTemplate.getExchangeRateWithApi(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
     }
 }
