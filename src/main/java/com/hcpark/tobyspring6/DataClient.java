5a0cce3973be587407a6372d4100961f6fdc25ff
hy2850 <hy2850@gmail.com>
2024-09-18 18:41:48 +0900

    DataAccessException과 예외 추상화 - DataIntegrityViolationException 캐치해서 처리

--- src/main/java/com/hcpark/tobyspring6/DataClient.java
+++ src/main/java/com/hcpark/tobyspring6/DataClient.java
@@ -4,13 +4,11 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.hcpark.tobyspring6.order.Order;
-import com.hcpark.tobyspring6.order.OrderRepository;
 import com.hcpark.tobyspring6.order.OrderRepositoryWithSpring;
 
 public class DataClient {
@@ -26,15 +24,19 @@ public class DataClient {
         OrderRepositoryWithSpring orderRepository = beanFactory.getBean(OrderRepositoryWithSpring.class);
         JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        new TransactionTemplate(transactionManager).execute(status -> {
-            Order newOrder = new Order("100", BigDecimal.TEN);
-            orderRepository.save(newOrder);
+        try {
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order newOrder = new Order("100", BigDecimal.TEN);
+                orderRepository.save(newOrder);
 
-            Order newOrder2 = new Order("100", BigDecimal.TEN);
-            orderRepository.save(newOrder2);
+                Order newOrder2 = new Order("100", BigDecimal.TEN);
+                orderRepository.save(newOrder2);
 
-            return null;
-        });
+                return null;
+            });
+        } catch (DataIntegrityViolationException e) {
+            System.out.println("중복된 유저 주문 처리");
+            // 예외 처리 비즈니스 로직 여기에
+        }
     }
-
 }
