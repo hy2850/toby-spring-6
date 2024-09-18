cf3a6923c627ccb99f68c114e1866e6166ee45df
hy2850 <hy2850@gmail.com>
2024-09-18 11:58:31 +0900

    ExRateExtractor 분리 - 콜백을 익명 클래스로 만들어서 메소드 인자로 넘기기

--- src/main/java/com/hcpark/tobyspring6/exrate/WebApiExRateProvider.java
+++ src/main/java/com/hcpark/tobyspring6/exrate/WebApiExRateProvider.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hcpark.tobyspring6.api.ApiExecutor;
+import com.hcpark.tobyspring6.api.ExRateExtractor;
 import com.hcpark.tobyspring6.api.SimpleApiExecutor;
 import com.hcpark.tobyspring6.payment.ExRateProvider;
 
@@ -23,10 +24,17 @@ public class WebApiExRateProvider implements ExRateProvider {
     @Override
     public BigDecimal getExchangeRate(String currency) {
         var url = "https://open.er-api.com/v6/latest/" + currency;
-        return getExchangeRateWithApi(url, new SimpleApiExecutor());
+
+        return getExchangeRateWithApi(url, new SimpleApiExecutor(), new ExRateExtractor() {
+            @Override
+            public ExchangeRateInfo extractExRateFrom(String response) throws JsonProcessingException {
+                var objectMapper = new ObjectMapper();
+                return objectMapper.readValue(response, ExchangeRateInfo.class);
+            }
+        });
     }
 
-    private static BigDecimal getExchangeRateWithApi(String url, ApiExecutor apiExecutor) {
+    private static BigDecimal getExchangeRateWithApi(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
         URI uri;
         try {
             uri = new URI(url);
@@ -43,18 +51,11 @@ public class WebApiExRateProvider implements ExRateProvider {
 
         ExchangeRateInfo exchangeRateInfo;
         try {
-            exchangeRateInfo = extractExRateFrom(response);
+            exchangeRateInfo = exRateExtractor.extractExRateFrom(response);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
 
         return exchangeRateInfo.rates().get(TARGET_CURRENCY);
-    }
-
-    private static ExchangeRateInfo extractExRateFrom(String response) throws JsonProcessingException {
-        ExchangeRateInfo exchangeRateInfo;
-        var objectMapper = new ObjectMapper();
-        exchangeRateInfo = objectMapper.readValue(response, ExchangeRateInfo.class);
-        return exchangeRateInfo;
     }
 }
