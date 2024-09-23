5d6749a128e2433c693f585e97bbe3e437c98830
hyuncheol-park <hyuncheol.park@navercorp.com>
2024-09-23 09:08:33 +0900

    트랜잭션 프록시 - OrderServiceTxProxy 생성해서 트랜잭션 적용

--- src/main/java/com/hcpark/tobyspring6/order/OrderServiceTxProxy.java
+++ src/main/java/com/hcpark/tobyspring6/order/OrderServiceTxProxy.java
@@ -3,8 +3,6 @@ package com.hcpark.tobyspring6.order;
 import java.math.BigDecimal;
 import java.util.List;
 
-import javax.naming.OperationNotSupportedException;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
@@ -12,43 +10,30 @@ import org.springframework.transaction.support.TransactionTemplate;
 @Service
 public class OrderServiceTxProxy implements OrderService {
 
-    private final OrderRepository orderRepository;
+    private final OrderService target;
     private final PlatformTransactionManager transactionManager;
 
-    public OrderServiceTxProxy(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
-        this.orderRepository = orderRepository;
+    public OrderServiceTxProxy(OrderService target, PlatformTransactionManager transactionManager) {
+        this.target = target;
         this.transactionManager = transactionManager;
     }
 
     @Override
     public Order createOrder(String no, BigDecimal total) {
-        Order newOrder = new Order(no, total);
-
-        //        orderRepository.save(newOrder);
-        //        return newOrder;
-
         return new TransactionTemplate(transactionManager).execute(status -> {
-            orderRepository.save(newOrder);
-            return newOrder;
+            return target.createOrder(no, total);
         });
     }
 
     @Override
     public List<Order> createOrders(List<OrderReq> orderReqs) {
-        //        return orderReqs.stream().map(orderReq -> createOrder(orderReq.no(), orderReq.total())).toList();
-
         return new TransactionTemplate(transactionManager).execute(status -> {
-            return orderReqs.stream().map(orderReq -> createOrder(orderReq.no(), orderReq.total())).toList();
+            return target.createOrders(orderReqs);
         });
     }
 
     @Override
     public Order getOrder(Long id) {
-        try {
-            return orderRepository.find(id);
-        } catch (OperationNotSupportedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        return target.getOrder(id);
     }
 }
